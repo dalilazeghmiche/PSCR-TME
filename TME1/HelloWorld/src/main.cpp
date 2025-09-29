04845a11eeee060a0078050d8c8d14f8c558d4ea
DALILA <dalilazeghmiche@gmail.com>
2025-09-30 00:03:07 +0200

    Ajout tableau et debug main.cpp

--- TME1/HelloWorld/src/main.cpp
+++ TME1/HelloWorld/src/main.cpp
@@ -1,4 +1,27 @@
 #include <iostream>
+using namespace std;
+
 int main() {
-std::cout << "Hello World!" << std::endl;
-return 0;}
+    int tab[10];
+
+    // Remplir le tableau avec les entiers 0 à 9
+    for (int i = 0; i < 10; ++i) {
+        tab[i] = i;
+    }
+
+    // Afficher le contenu du tableau
+    cout << "Contenu du tableau : ";
+    for (int i = 0; i < 10; ++i) {
+        cout << tab[i] << " ";
+    }
+    cout << endl;
+
+    // Vérification
+    for (size_t i = 9; i > 0; i--) {   // ⚠ Correction : i > 0 pour éviter sous-flux
+        if (tab[i] - tab[i-1] != 1) {
+            cout << "probleme !" << endl;
+        }
+    }
+
+    return 0;
+}
